50fe8fa86f0844a70af0391fa29f653ba60d4985
sxy15 <sxy_19921115@163.com>
2023-10-09 21:23:35 +0800

    feat: cli version

--- packages/mumu-cli/src/index.ts
+++ packages/mumu-cli/src/index.ts
@@ -1,1 +1,12 @@
-console.log('index');
+import fs from 'node:fs';
+import { URL, fileURLToPath } from 'node:url';
+import { logger } from 'rslog';
+
+const pkgPath = fileURLToPath(new URL('../package.json', import.meta.url));
+const pkgJson = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'));
+
+export const cliVersion: string = pkgJson.version;
+
+logger.greet(`mumu cli v${cliVersion}`);
+
+process.env.MUMU_CLI_VERSION = cliVersion;
