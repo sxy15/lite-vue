16932b823d497a652e8a20f160e2fe471ca95844
sxy15 <sxy_19921115@163.com>
2023-10-09 22:41:34 +0800

    feat: commit lint

--- packages/mumu-cli/src/cli.ts
+++ packages/mumu-cli/src/cli.ts
@@ -1,7 +1,16 @@
 import { Command } from 'commander';
+import { cliVersion } from './index.js';
 
 const program = new Command();
 
-program.version(`@mumu/cli ${'2.2.0'}`);
+program.version(`@mumu/cli ${cliVersion}`);
+
+program
+  .command('commit-lint <gitParams>')
+  .description('lint commit message')
+  .action(async (gitParams) => {
+    const { commitLint } = await import('./commands/commit-lint.js');
+    return commitLint(gitParams);
+  });
 
 program.parse();
