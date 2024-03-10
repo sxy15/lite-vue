187a16db160e88704eee76eb122a3db45848f27f
sxy15 <sxy_19921115@163.com>
2024-03-10 21:33:22 +0800

    rm *

--- packages/sxyz/src/button/demo/index.vue
+++
@@ -1,7 +0,0 @@
-<script setup></script>
-
-<template>
-  <demo-block> </demo-block>
-</template>
-
-<style lang="less"></style>
--- packages/sxyz/src/utils/props.ts
+++
@@ -1,6 +0,0 @@
-import type { PropType } from 'vue';
-
-export const makeStringProp = <T>(defaultVal: T) => ({
-  type: String as unknown as PropType<T>,
-  default: defaultVal,
-});
