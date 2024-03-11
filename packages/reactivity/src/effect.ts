3819420e7b7384f8b44ceb1cf1527fc2aae1e3c3
sxy15 <sxy_19921115@163.com>
2024-03-11 15:47:36 +0800

    feat: reactive

---
+++ packages/reactivity/src/effect.ts
@@ -0,0 +1,66 @@
+import { isArray } from "@vue/shared"
+import { Dep, createDep } from "./dep"
+
+type KeyToDepMap = Map<any, Dep>
+const targetMap = new WeakMap<any, KeyToDepMap>()
+
+export function effect<T = any>(fn: () => T) {
+  const _effect = new ReactiveEffect(fn)
+
+  _effect.run()
+}
+
+export let activeEffect: ReactiveEffect | undefined
+
+export class ReactiveEffect<T = any> {
+  constructor(public fn: () => T) {}
+
+  run() {
+    activeEffect = this
+
+    return this.fn()
+  }
+}
+
+export function track(target: object, key: unknown) {
+  if(!activeEffect) return 
+  let depsMap = targetMap.get(target)
+  if(!depsMap) {
+    targetMap.set(target, (depsMap = new Map()))
+  }
+
+  let dep = depsMap.get(key)
+  if(!dep) {
+    depsMap.set(key, (dep = createDep()))
+  }
+
+  trackEffects(dep) // 将当前的 activeEffect 添加到 dep 中
+}
+
+export function trackEffects(dep: Dep) {
+  dep.add(activeEffect!)
+}
+
+export function trigger(target: object, key: unknown, value: unknown) {
+  const depsMap = targetMap.get(target)
+
+  if(!depsMap) return
+
+  const dep = depsMap.get(key) as Dep
+
+  if(!dep) return
+
+  triggerEffects(dep) // 依次执行 dep 中的 effect
+}
+
+export function triggerEffects(dep: Dep) {
+  const effects = isArray(dep) ? dep : [...dep]
+
+  for(let effect of effects) {
+    triggerEffect(effect)
+  }
+}
+
+export function triggerEffect(effect: ReactiveEffect) {
+  effect.run()
+}
